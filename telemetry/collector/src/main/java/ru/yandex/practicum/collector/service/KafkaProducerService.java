d4bcdace1fa23a9251b56453ca512b21668ec971
foolvan <ivanmedvedev1357@gmail.com>
2026-05-11 19:24:22 +0300

    wedfrt

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/KafkaProducerService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/KafkaProducerService.java
@@ -1,38 +1,49 @@
 package ru.yandex.practicum.collector.service;
 
-import org.apache.avro.io.EncoderFactory;
+import jakarta.annotation.PreDestroy;
 import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 @Service
 public class KafkaProducerService {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaProducerService.class);
-    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+    private final KafkaProducer<String, byte[]> producer;
 
-    public KafkaProducerService(KafkaTemplate<String, byte[]> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    public KafkaProducerService(@Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
+        this.producer = new KafkaProducer<>(props);
     }
 
     public void send(String topic, String key, SpecificRecordBase event) {
         try {
             byte[] bytes = serializeAvro(event);
-            kafkaTemplate.send(topic, key, bytes)
-                    .whenComplete((result, ex) -> {
-                        if (ex == null) {
-                            log.info("Sent event to topic {}: {}", topic, event);
-                        } else {
-                            log.error("Failed to send event to topic {}", topic, ex);
-                        }
-                    });
+            ProducerRecord<String, byte[]> record = new ProducerRecord<>(topic, key, bytes);
+            producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    log.info("Sent event to topic {}: offset={}", topic, metadata.offset());
+                } else {
+                    log.error("Failed to send event to topic {}", topic, exception);
+                }
+            });
         } catch (IOException e) {
             log.error("Failed to serialize Avro event", e);
             throw new RuntimeException("Avro serialization failed", e);
@@ -46,6 +57,13 @@ public class KafkaProducerService {
             writer.write(record, encoder);
             encoder.flush();
             return out.toByteArray();
+        }
+    }
+
+    @PreDestroy
+    public void close() {
+        if (producer != null) {
+            producer.close();
         }
     }
 }
