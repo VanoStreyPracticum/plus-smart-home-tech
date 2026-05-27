bcf4667ba7e67bd7de0705fe589212e3e85e050a
foolvan <ivanmedvedev1357@gmail.com>
2026-05-27 05:36:42 +0300

    Auto-start hub-router on port 59090 from Analyzer for CI

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerApplication.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerApplication.java
@@ -7,10 +7,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 import ru.yandex.practicum.analyzer.processor.HubEventProcessor;
 import ru.yandex.practicum.analyzer.processor.SnapshotProcessor;
 
+import java.io.File;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class AnalyzerApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // Запускаем hub-router.jar, если он есть в корне проекта
+        File hubRouterJar = new File("hub-router.jar");
+        if (hubRouterJar.exists()) {
+            ProcessBuilder pb = new ProcessBuilder("java", "-jar", "hub-router.jar", "--server.port=59090");
+            pb.inheritIO();
+            pb.start();
+            // Даём время на старт
+            Thread.sleep(5000);
+        }
+
         ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
 
         HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
