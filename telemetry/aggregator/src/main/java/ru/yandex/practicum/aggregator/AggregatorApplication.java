1f9122f6ebf0380095b82f9b9f12106ecfbc6b6c
foolvan <ivanmedvedev1357@gmail.com>
2026-05-27 05:04:39 +0300

    sprint 20

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/AggregatorApplication.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/AggregatorApplication.java
@@ -2,10 +2,15 @@ package ru.yandex.practicum.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class AggregatorApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AggregatorApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext context = SpringApplication.run(AggregatorApplication.class, args);
+        // Блокируем основной поток, чтобы приложение не завершалось сразу.
+        // В реальном агрегаторе здесь будет запуск цикла опроса Kafka.
+        new CountDownLatch(1).await();
     }
 }
