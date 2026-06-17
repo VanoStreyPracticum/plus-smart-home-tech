87a86711b11e548fd39922314e19e8a9e6f065bb
foolvan <ivanmedvedev1357@gmail.com>
2026-06-17 14:04:21 +0300

    first commit

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/model/WarehouseEntity.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/model/WarehouseEntity.java
@@ -1,16 +1,20 @@
 package ru.yandex.practicum.warehouse.model;
+
 import jakarta.persistence.*;
 import lombok.*;
+import java.util.UUID;
 
 @Entity
 @Table(name = "warehouse_items")
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class WarehouseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
     @Column(unique = true)
-    private Long productId;
+    private UUID productId;
+
     private Integer quantity;
     private Double width;
     private Double height;
--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/repository/WarehouseRepository.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/repository/WarehouseRepository.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.warehouse.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.warehouse.model.WarehouseEntity;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface WarehouseRepository extends JpaRepository<WarehouseEntity, Long> {
-    Optional<WarehouseEntity> findByProductId(Long productId);
+public interface WarehouseRepository extends JpaRepository<WarehouseEntity, UUID> {
+    Optional<WarehouseEntity> findByProductId(UUID productId);
 }
